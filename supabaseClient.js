b5319df25a703d7c563012bd6331fc84cb087d19
williamguto0911-design <william.guto0911@gmail.com>
2025-08-14 19:29:40 -0300

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://ahuvldyeownupkgjqusl.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodXZsZHllb3dudXBrZ2pxdXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyMDc3MDUsImV4cCI6MjA3MDc4MzcwNX0.pkDtP_ziDZzh2QNSN0FCDMA4PUbrgdvTPt-6O5W9k';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodXZsZHllb3dudXBrZ2pxdXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyMDc3MDUsImV4cCI6MjA3MDc4MzcwNX0.pkDtP_ziDZzh2QNSN0FCDMA4PUbrgdvTPtP6-6O5W9k';
 
 // Cria a instância do cliente Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
